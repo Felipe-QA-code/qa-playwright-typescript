7a3ef64566b6f3bcc916495d4373cc88e7cd1ffd
LazloQA <lazlo.qa@gmail.com>
2026-09-05 06:49:27 -0400

    Minor improvements

--- tests/api_test_post.spec.ts
+++ tests/api_test_post.spec.ts
@@ -1,25 +1,35 @@
 import { test, expect } from '@playwright/test'
 
-test (' POST user', async ({request})=>{
+test ('Execute a POST method for user', async ({request})=>{
 
+    // Test Data
+    const nameParameter = 'felipe'
+    const userNameParameter = 'felipeqa'
+    const emailParameter = 'felipe@test.com'
+
+    // Execute POST method
     const response = await request.post('https://jsonplaceholder.typicode.com/users',
         {
             data:{
-                name:'felipe',
-                username:'felipeqa',
-                email:'felipe@test.com'
+                name:nameParameter,
+                username:userNameParameter,
+                email:emailParameter
             }
         }
     )
+
+    // Response status code validation
     expect (response.status()).toBe(201)
+
+    // Response body validation
     const body = await response.json()
     console.log(body)
     console.log(typeof body.email)
 
-    expect (body.name).toBe('felipe')
-    expect (body.username).toBe('felipeqa')
-    expect (body.email).toBe('felipe@test.com')
+    // Response body attributes validation
+    expect (body.name).toBe(nameParameter)
+    expect (body.username).toBe(userNameParameter)
+    expect (body.email).toBe(emailParameter)
     expect(body.id).toBeDefined()
-    expect(body.id).toEqual(expect.any(Number))
-    
+    expect(body.id).toEqual(expect.any(Number))    
 })
